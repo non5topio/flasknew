8d9e4b850e9505236897f28e413f25281f5ff74b
Nikhil <nikhil.murade@nonstopio.com>
2025-04-07 15:32:48 +0530

    add proper build-test-env-docker-statge.sh file

--- tool/build-test-env-docker-stage.sh
+++ tool/build-test-env-docker-stage.sh
@@ -5,14 +5,14 @@ set -e -o pipefail
 PROJECT_NAME="$(
   jq \
     --raw-output \
-    .increase-coverage.project-name \
+    '."increase-coverage"."project-name"' \
     test-gen-config.json
 )"
 
 TEST_ENV_DOCKER_STAGE="$(
   jq \
     --raw-output \
-    .increase-coverage.test-env-docker-stage \
+    '."increase-coverage"."test-env-docker-stage"' \
     test-gen-config.json
 )"
 
@@ -21,4 +21,3 @@ docker build \
   --platform linux/amd64 \
   --tag "$PROJECT_NAME:$TEST_ENV_DOCKER_STAGE" \
   .
-
